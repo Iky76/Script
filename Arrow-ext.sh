e1358ed40f16fd98a215ca8e22c6106452480e14
Iky76 <yuyenem@gmail.com>
2026-03-19 06:32:09 +0700

    Optimize git clone commands in Arrow-ext.sh
    Updated git clone commands to use --depth=1 for faster cloning.

--- Arrow-ext.sh
+++ Arrow-ext.sh
@@ -4,15 +4,16 @@ rm -rf prebuilts/clang/host/linux-x86
 repo init --no-repo-verify --git-lfs -u https://github.com/ArrowOS-T/android_manifest.git -b arrow-13.1_ext -g default,-mips,-darwin,-notdefault
 /opt/crave/resync.sh || repo sync
 
-git clone https://github.com/Iky76/device_xiaomi_mido.git -b arrow device/xiaomi/mido
-git clone https://github.com/Iky76/vendor_xiaomi_mido_A13.git -b android-13 vendor/xiaomi/mido
-git clone https://github.com/PixysOS-Devices/kernel_xiaomi_mido.git -b thirteen kernel/xiaomi/mido
+git clone --depth=1 https://github.com/Iky76/device_xiaomi_mido.git -b arrow device/xiaomi/mido
+git clone --depth=1 https://github.com/Iky76/vendor_xiaomi_mido_A13.git -b android-13 vendor/xiaomi/mido
+git clone --depth=1 https://github.com/PixysOS-Devices/kernel_xiaomi_mido.git -b thirteen kernel/xiaomi/mido
 
 git clone https://github.com/LineageOS/android_hardware_qcom-caf_common.git -b lineage-20.0 hardware/qcom-caf/common
 git clone https://github.com/LineageOS/android_hardware_qcom_bt.git -b lineage-20.0-caf hardware/qcom-caf/bt
 
 export BUILD_USERNAME=Rizj
 export BUILD_HOSTNAME=DooPrjkt
+export SOONG_ALLOW_MISSING_DEPENDENCIES=true
 
 . build/envsetup.sh
 lunch lineage_mido-userdebug
