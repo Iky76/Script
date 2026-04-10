0af8a9044b8ac30ab4229b80cd91765b4b43a51c
Iky76 <yuyenem@gmail.com>
2026-04-10 17:07:55 +0700

    Add script to initialize LineageOS build for Mido
    This script initializes the LineageOS build environment for the Xiaomi Mido device, including cloning necessary repositories and setting up build variables.

---
+++ lineage-20.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+rm -rf prebuilts/clang/host/linux-x86
+
+repo init --no-repo-verify --git-lfs -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
+/opt/crave/resync.sh || repo sync
+
+git clone --depth=1 https://github.com/Iky76/device_xiaomi_mido.git -b lineage-20.0 device/xiaomi/mido
+git clone --depth=1 https://github.com/PixysOS-Devices/vendor_xiaomi_mido -b thirteen vendor/xiaomi/mido
+git clone --depth=1 https://github.com/PixysOS-Devices/kernel_xiaomi_mido.git -b thirteen kernel/xiaomi/mido
+
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-20 hardware/xiaomi
+
+export BUILD_USERNAME=mambo
+export BUILD_HOSTNAME=crave
+
+. build/envsetup.sh
+lunch lineage_mido-userdebug
+m bacon
