105631bb3a1230051760b14790bf71f1b6a6a363
Iky76 <yuyenem@gmail.com>
2026-04-11 22:56:10 +0700

    Clean up directories and update hardware repo links
    Remove unnecessary directories and update hardware cloning.

--- lineage-20.sh
+++ lineage-20.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 rm -rf prebuilts/clang/host/linux-x86
 rm -rf device/xiaomi/mido
+rm -rf hardware/xiaomi
+rm -rf vendor/xiaomi/mido
+rm -rf kernel/xiaomi/mido
 
 repo init --no-repo-verify --git-lfs -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
 /opt/crave/resync.sh || repo sync
@@ -8,8 +11,6 @@ repo init --no-repo-verify --git-lfs -u https://github.com/LineageOS/android.git
 git clone --depth=1 https://github.com/Iky76/device_xiaomi_mido.git -b lineage-20.0 device/xiaomi/mido
 git clone --depth=1 https://github.com/PixysOS-Devices/vendor_xiaomi_mido -b thirteen vendor/xiaomi/mido
 git clone --depth=1 https://github.com/PixysOS-Devices/kernel_xiaomi_mido.git -b thirteen kernel/xiaomi/mido
-
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-20 hardware/xiaomi
 
 export BUILD_USERNAME=mambo
 export BUILD_HOSTNAME=crave
